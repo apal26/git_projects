0d63f72e41f09669dff91134e240f54e9d0b4d6d
RhymerRambler <ashutoshpal2006@gmail.com>
2025-12-29 20:12:22 +0530

    fixed multiple deprecated API and other warnings; using dotenv to hide the API key

--- agent/agent_gemini_1.py
+++ agent/agent_gemini_1.py
@@ -4,8 +4,14 @@ from langchain_classic.prompts import PromptTemplate
 from langchain_classic.tools import Tool
 from langchain_classic.agents import initialize_agent, AgentType
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 #gemini key created from google AI studio
-mykey = "AIzaSyDub1undfAxUNgoZFjbP1a2qMsWbG1X2co"
+mykey = os.getenv("GOOGLE_API_KEY")
+
 #initialize llm model
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=mykey)
 
@@ -19,12 +25,17 @@ template1 = """
 """
 #prepare the prompt and setup a chain without tool
 prompt1 = PromptTemplate(template=template1, input_variables=["question"])
-chain = LLMChain(prompt=prompt1, llm=llm)
+chain = prompt1 | llm
+
+print(type(chain))
 # launch a query
 query1 = "does science has a path to self-realization? Give a short answer."
-response1 = chain.run(question=query1)
-print(f"Agent response: {response1}")
+response1 = chain.invoke({"question" : query1})
+print(type(response1))
+print(f"Agent response: {response1.content}")
 #######################################
+
+
 
 #######################################
 # **** Agent with tool use capability
@@ -32,7 +43,8 @@ print(f"Agent response: {response1}")
 import yfinance as yf
 def get_stock_value(stock_symbol):
   stock = yf.Ticker(stock_symbol)
-  return stock.history(period="1d")["Close"][0]
+  return stock.history(period="1d")["Close"].iloc[0]
+
 # make the tool available to the llm model
 tool = Tool.from_function(func=get_stock_value, name="GetStockVal", description="Gets the stock price for a given symbol, like SNPS")
 tools = [tool]
@@ -43,7 +55,11 @@ agent = initialize_agent(
     verbose=False,
     system_message="You are a helpful financial assistant and tell the stock-prices using the GetStockVal tool."
 )
+# initialize_agent with type ZERO_SHOT_REACT_DESCRIPTION has its own in-built
+# prompt template:
+#print(f"in-built prompt template: {agent.agent.llm_chain.prompt.template}")
+#
 # launch a query to chain_with_tool
 query2 = "what is the stock-price of SNPS?"
-response2 = agent.run(query2)
+response2 = agent.invoke(query2)
 print(f"Agent response: {response2}")
